2bef497a0527936102ab85dcb9da47dee7a581fc
jjanovskis <j.janovskis@savethechildren.org.uk>
2013-04-05 18:08:09 +0100

    Main commit - first

---
+++ modules/cool_form/cool_form_page.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<?php //dsm($variables); ?>
+
+<div class="custom-form">
+    <?php print $cool_form; ?>
+</div>
